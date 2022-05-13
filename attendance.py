70ebe7eebe2568ae6e5533f246d3b8ac0c6ab999
David <67174920+davidr-AI@users.noreply.github.com>
2022-05-13 15:08:15 -0400

    Add files via upload

---
+++ attendance.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+# create a list that can fetch images automatically
+path = "imagesAttendance"
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+
+# use these names and import them one by one
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    # splitting the name...instead of name.jpg it will give name and without jpg
+    classNames.append(os.path.splitext(cl)[0])
+
+print(classNames)
+
+
+# encoding process
+# once we import our image we have to find enode for each of them
+# we create simple function that will do encode for us
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%B/%A/%Y, %H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
+encodeListKnown = findEncodings(images)
+print(len(encodeListKnown))
+
+cap = cv2.VideoCapture(0)
+# get each frame one by one
+
+while True:
+    # will give our image
+    success, img = cap.read()
+    # reduce size of image which will help speeding process
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    # converting to RGB
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # find the encoding of our webcam
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    # finding the matches
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1, x2, y2, x1 = faceLoc
+            # y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
+
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
